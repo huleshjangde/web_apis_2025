dee07bc163f7f8660cb9b0dbb5cf73f0b0a2bcdb
huleshjangde <huleshjangde1@gmail.com>
2025-01-21 20:35:29 +0530

    feat: docs viewer by iframe ms doc embed

--- app/page.tsx
+++ app/page.tsx
@@ -1,53 +1,22 @@
-'use client';
-import CustomCursor from '@/components/CustomCursor';
-import React, { useState } from 'react';
+const Test = () => {
+  const documentUrl =
+    'https://zhymzpncibzsfyqxutct.supabase.co/storage/v1/object/sign/files/pdfs/f30ffb4e-e884-4a73-b5a8-9b6af4d035e5/Atrocities Human.docx?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJmaWxlcy9wZGZzL2YzMGZmYjRlLWU4ODQtNGE3My1iNWE4LTliNmFmNGQwMzVlNS9BdHJvY2l0aWVzIEh1bWFuLmRvY3giLCJpYXQiOjE3Mzc0NDEwNzEsImV4cCI6MjczNzQ0MTA3OH0.oAYW8erBY3uZpLmNoDWTmQDTwbQAdiHTX8zG8AwzQzQ';
 
-const CursorWrapper: React.FC = () => {
-  const [cursorColor, setCursorColor] = useState(
-    'bg-gradient-to-r from-pink-500 to-yellow-500',
-  );
-  const [cursorText, setCursorText] = useState('');
-
-  const handleMouseEnter = (event: React.MouseEvent<HTMLDivElement>) => {
-    const target = event.currentTarget.getBoundingClientRect();
-    const entryFromLeft =
-      event.clientX - target.left < target.right - event.clientX;
-    console.log(
-      'clientX',
-      event.clientX,
-      'T-left: ',
-      target.left,
-      'T-right: ',
-      target.right,
-      'entryFromLeft: ',
-      entryFromLeft,
-    );
-
-    if (entryFromLeft) {
-      setCursorColor('bg-gradient-to-r from-green-500 to-blue-500');
-      setCursorText('From Left');
-    } else {
-      setCursorColor('bg-gradient-to-r from-red-500 to-purple-500');
-      setCursorText('From Right');
-    }
-  };
-
-  const handleMouseLeave = () => {
-    setCursorColor('bg-gradient-to-r from-pink-500 to-yellow-500');
-    setCursorText('');
-  };
+  const viewerUrl = `https://view.officeapps.live.com/op/embed.aspx?src=${encodeURIComponent(
+    documentUrl,
+  )}`;
 
   return (
-    <div>
-      <CustomCursor color={cursorColor} text={cursorText} />
-      <div
-        className="mx-auto my-20 flex h-64 w-64 items-center justify-center bg-gray-200"
-        onMouseEnter={handleMouseEnter}
-        onMouseLeave={handleMouseLeave}>
-        Hover over this box
-      </div>
-    </div>
+    <iframe
+      src={viewerUrl}
+      style={{
+        width: '100vw',
+        height: '100vh',
+        border: 'none',
+      }}
+      title="Office Document Viewer"
+    />
   );
 };
 
-export default CursorWrapper;
+export default Test;
