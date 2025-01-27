cc0cad39916e90a6865d298bb282b210f14994a3
huleshjangde <huleshjangde1@gmail.com>
2025-01-27 23:15:44 +0530

    zustand add

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import { Counter } from '@/components/Counter';
+
 const Test = () => {
   const documentUrl =
     'https://zhymzpncibzsfyqxutct.supabase.co/storage/v1/object/sign/files/pdfs/f30ffb4e-e884-4a73-b5a8-9b6af4d035e5/Atrocities Human.docx?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJmaWxlcy9wZGZzL2YzMGZmYjRlLWU4ODQtNGE3My1iNWE4LTliNmFmNGQwMzVlNS9BdHJvY2l0aWVzIEh1bWFuLmRvY3giLCJpYXQiOjE3Mzc0NDEwNzEsImV4cCI6MjczNzQ0MTA3OH0.oAYW8erBY3uZpLmNoDWTmQDTwbQAdiHTX8zG8AwzQzQ';
@@ -13,17 +15,20 @@ const Test = () => {
   )}`;
 
   return (
-    <iframe
-      src={`https://view.officeapps.live.com/op/embed.aspx?src=${encodeURIComponent(
-        documentUrl,
-      )}`}
-      style={{
-        width: '100vw',
-        height: '100vh',
-        border: 'none',
-      }}
-      title="Office Document Viewer"
-    />
+    // <iframe
+    //   src={`https://view.officeapps.live.com/op/embed.aspx?src=${encodeURIComponent(
+    //     documentUrl,
+    //   )}`}
+    //   style={{
+    //     width: '100vw',
+    //     height: '100vh',
+    //     border: 'none',
+    //   }}
+    //   title="Office Document Viewer"
+    // />
+    <>
+      <Counter />
+    </>
   );
 };
 
