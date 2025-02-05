5dd71f5ab6181e37246173dabe610e5332753e3b
huleshjangde <huleshjangde1@gmail.com>
2025-02-05 22:11:53 +0530

    add:file

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,14 @@
 import { Counter } from '@/components/Counter';
 
 const Test = () => {
+  const documentUrl =
+    'https://zhymzpncibzsfyqxutct.supabase.co/stohggrage/v1/object/sign/files/pdfs/f30ffb4e-e884-4a73-b5a8-9b6af4d035e5/Atrocities Human.docx?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJmaWxlcy9wZGZzL2YzMGZmYjRlLWU4ODQtNGE3My1iNWE4LTliNmFmNGQwMzVlNS9BdHJvY2l0aWVzIEh1bWFuLmRvY3giLCJpYXQiOjE3Mzc0NDEwNzEsImV4cCI6MjczNzQ0MTA3OH0.oAYW8erBY3uZpLmNoDWTmQDTwbQAdiHTX8zG8AwzQzQ';
+  const doddcumentUrl =
+    'https://zhymzpncibzsfyqxutct.supabase.co/stohggrage/v1/object/sign/files/pdfs/f30ffb4e-e884-4a73-b5a8-9b6af4d035e5/Atrocities Human.docx?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJmaWxlcy9wZGZzL2YzMGZmYjRlLWU4ODQtNGE3My1iNWE4LTliNmFmNGQwMzVlNS9BdHJvY2l0aWVzIEh1bWFuLmRvY3giLCJpYXQiOjE3Mzc0NDEwNzEsImV4cCI6MjczNzQ0MTA3OH0.oAYW8erBY3uZpLmNoDWTmQDTwbQAdiHTX8zG8AwzQzQ';
+
+  const nochange = 'sorry';
+  const nocsdshange = 'sorry';
+
   return (
     // <iframe
     //   src={`https://view.officeapps.live.com/op/embed.aspx?src=${encodeURIComponent(
