d1725655857a6463b35cad39e97526f258e6db8a
hulesh jangde <63199618+huleshjangde@users.noreply.github.com>
2025-02-10 23:07:53 +0530

    Update utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,6 +4,3 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
-const h = 'dsaas';
-const had = 'dsaas';
-const hd = 'dsaas';
